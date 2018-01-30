87052e65ba47edc9be978e9041d69afd70dc7e70
Sylwia Zak <zak.sylwia97@gmail.com>
2018-01-30 23:15:51 +0100

    first start

---
+++ src/main/java/DropBoxService.java
@@ -0,0 +1,23 @@
+import com.dropbox.core.DbxException;
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.v2.DbxClientV2;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class DropBoxService {
+
+    private DbxClientV2 client;
+
+    public DropBoxService(String accessToken) {
+        DbxRequestConfig config = new DbxRequestConfig("First Dbx");
+        client= new DbxClientV2(config,accessToken);
+    }
+
+    public void send(File file, String targetPath) throws IOException, DbxException {
+        try (FileInputStream inputStream = new FileInputStream(file)) {
+            client.files().uploadBuilder(targetPath).uploadAndFinish(inputStream);
+        }
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,16 @@
 import java.io.IOException;
+import java.nio.file.WatchService;
 
 
 public class Main {
     public static void main(String[] args) {
 
+        WatcherService watcherService= new WatcherService();
+
+        try {
+            watcherService.watch("src\\main\\resources\\source");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
