e5d66125270ec511f51b409f6bb1c574fb3be4db
Sylwia Zak <zak.sylwia97@gmail.com>
2018-01-30 12:34:03 +0100

    after generating token

--- src/main/java/DrobBoxService.java
+++ src/main/java/DrobBoxService.java
@@ -7,32 +7,21 @@ import com.dropbox.core.v1.DbxWriteMode;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Locale;
 
 public class DrobBoxService {
 
     private DbxClientV1 clientV1;
 
-    public DrobBoxService(DbxRequestConfig config, String accessToken) {
-        new DbxClientV1(config,accessToken);
+    public DrobBoxService(String accessToken) {
+        DbxRequestConfig config = new DbxRequestConfig("First Dbx", Locale.getDefault().toString());
+        clientV1= new DbxClientV1(config,accessToken);
     }
 
     public void send(File file, String targetPath) throws IOException, DbxException {
-        FileInputStream inputStream= new FileInputStream(file);
-        try {
+        try (FileInputStream inputStream = new FileInputStream(file)) {
             DbxEntry.File uploadedFile = clientV1.uploadFile(targetPath, DbxWriteMode.add(), file.length(), inputStream);
             System.out.println("Uploaded: " + uploadedFile.toString());
-        }
-        finally {
-            inputStream.close();
-        }
-    }
-
-    public void informationAboutTheFile (DbxClientV1 client) throws DbxException {
-        this.clientV1= client;
-        DbxEntry.WithChildren listing = client.getMetadataWithChildren("/");
-        System.out.println("Files in the root path:");
-        for (DbxEntry child : listing.children) {
-            System.out.println("	" + child.name + ": " + child.toString());
         }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,37 +1,8 @@
-import com.dropbox.core.*;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.time.LocalDate;
-import java.util.Locale;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-
-    final String APP_KEY = "Insert app key";
-    final String APP_SECRET = "Insert app secret";
-
-        DbxAppInfo appInfo= new DbxAppInfo(APP_KEY, APP_SECRET);
-
-        DbxRequestConfig config= new DbxRequestConfig("First Dbx", Locale.getDefault().toString());
-        DbxWebAuthNoRedirect webAuth= new DbxWebAuthNoRedirect(config, appInfo);
-
-        String authorizeUrl= webAuth.start();
-        System.out.println("1. Go to: " + authorizeUrl);
-        System.out.println("2. Click \" Allow\" ");
-        System.out.println("3. Copy the authorization code.");
-        String code= new BufferedReader(new InputStreamReader(System.in)).readLine().trim();
-        String accessToken= null;
-        try {
-            DbxAuthFinish authFinish= webAuth.finish(code);
-            accessToken= authFinish.getAccessToken();
-        } catch (DbxException e) {
-            System.out.println("invalid authorization code");
-            e.printStackTrace();
-        }
-
+    public static void main(String[] args) {
 
     }
 }
